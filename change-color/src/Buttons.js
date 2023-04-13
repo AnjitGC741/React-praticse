5cc1ed248d66821f853e17d6a1a2edd43bf3d525
ANjitGC741 <anzit741@gmail.com>
2023-04-13 21:19:51 +0545

    change

--- change-color/src/Buttons.js
+++ change-color/src/Buttons.js
@@ -5,40 +5,20 @@ class Buttons extends Component{
         this.state={
             color:"white",
         };
-        this.changeColorToRed = this.changeColorToRed.bind(this);
-        this.changeColorToGreen = this.changeColorToGreen.bind(this);
-        this.changeColorToBlue = this.changeColorToBlue.bind(this);
-        this.changeColorToYellow = this.changeColorToYellow.bind(this);
-        this.changeColorToBlack = this.changeColorToBlack.bind(this);
+        this.changeColor = this.changeColor.bind(this);
     }
-    changeColorToRed(){
-        this.setState({color:"red"});
-        document.body.style.backgroundColor = this.state.color;
-    }
-    changeColorToGreen(){
-        this.setState({color:"green"});
-        document.body.style.backgroundColor = this.state.color;
-    }
-    changeColorToBlue(){
-        this.setState({color:"blue"});
-        document.body.style.backgroundColor = this.state.color;
-    }
-    changeColorToYellow(){
-        this.setState({color:"yellow"});
-        document.body.style.backgroundColor = this.state.color;
-    }
-    changeColorToBlack(){
-        this.setState({color:"black"});
+    changeColor(colorName){
+        this.setState({color:colorName});
         document.body.style.backgroundColor = this.state.color;
     }
     render(){
         return(
             <div>
-            <button onClick={this.changeColorToRed}>Red</button>
-            <button onClick={this.changeColorToGreen}>Green</button>
-            <button onClick={this.changeColorToBlue}>Blue</button>
-            <button onClick={this.changeColorToYellow}>Yellow</button>
-            <button onClick={this.changeColorToBlack}>Black</button>
+            <button onClick={()=>this.changeColor("red")}>Red</button>
+            <button onClick={()=>this.changeColor("green")}>Green</button>
+            <button onClick={()=>this.changeColor("blue")}>Blue</button>
+            <button onClick={()=>this.changeColor("yellow")}>Yellow</button>
+            <button onClick={()=>this.changeColor("black")}>Black</button>
             </div>
         );
     }
