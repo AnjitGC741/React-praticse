08a7a76ce141a9424efe6eeb2cdbcf92146a26b4
ANjitGC741 <anzit741@gmail.com>
2023-04-21 20:01:32 +0545

    user-card

--- user-card/src/App.js
+++ user-card/src/App.js
@@ -1,26 +1,34 @@
-import './App.css';
-import React from 'react';
-class App extends React.Component{
-  constructor(){
+import "./App.css";
+import React from "react";
+class App extends React.Component {
+  constructor() {
     super();
-    this.state={
-        name:"Anjit",
+    this.state = {
+      name: "Shitiz Adhikari",
     };
-}
-  render(){
+  }
+  render() {
     const widthValue = {
-        width : '60%'
+      width: "60%",
     };
-    return(
-        <>
-         <h1>{this.state.name}</h1>
-        <h1>{this.state.name[0]}</h1>
-        <div className='ProgressBarBox'>
-              <div className='progressBarValue' style={widthValue}>
-
-              </div>
+    return (
+      <div className="main-user-card-box">
+        <div className="wrapper">
+          <div className="box-one">
+            <p>{this.state.name[0].toUpperCase()}</p>
+          </div>
+          <div className="box-two">
+            <p className="user-name">
+              {this.state.name}
+              <span className="green-dot"></span>
+            </p>
+            <p className="user-profession">Student</p>
+            <div className="ProgressBarBox">
+              <div className="progressBarValue" style={widthValue}></div>
+            </div>
+          </div>
         </div>
-        </>
+      </div>
     );
   }
 }
