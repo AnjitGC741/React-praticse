836fc410068a0e55681171c5e71d7dfaa332fffa
ANjitGC741 <anzit741@gmail.com>
2023-04-23 21:40:24 +0545

    change

--- employee-record/src/index.js
+++ employee-record/src/index.js
@@ -2,6 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import '../src/assests/Css/index.css';
+import {
+    createBrowserRouter,
+    RouterProvider,
+  } from "react-router-dom";
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element:<App/>,
+    },
+    {
+        path:'/about',
+        element:<div>This is about page</div>
+    }
+  ]);
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render( <App />);
+root.render( <RouterProvider router={router} />);
 
